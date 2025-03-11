d716a0bbbaa790290b086757ed1cf41a4e8a83cd
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-03-10 21:24:16 -0300

    feat: adding router view avatar image

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import Express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
+import uploadConfig from './config/upload'
 import './database';
 
 
@@ -10,3 +11,4 @@ export const app = Express();
 app.use(Express.json());
 app.use(cors());
 app.use('/', routes);
+app.use('/files', Express.static(uploadConfig.directory))
